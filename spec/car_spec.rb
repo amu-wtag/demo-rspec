261ab90584e830f9c5ecaf8013ce9529f2d95fd7
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-06 11:31:18 +0600

    added before hooks and let

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,12 +2,22 @@ require 'car'
 
 RSpec.describe 'Car' do
   describe 'attributes' do
-    car = Car.new
+    # before hooks
+    # before(:example) do
+    #   @car = Car.new
+    #   puts 'before example***'
+    # end
+
+    # let is better than "before" for setting up instance variables
+    let(:car) { Car.new }
+
+    # subject { Car.new }
+    # This does the same as let(:subject) { Car.new }
 
     # Skipping the test with prepending x before it
     xit 'allows reading and writing for :make' do
       car.make = 'Test'
-      expect(car.make).to eq('Test')
+      # expect(car.make).to eq('Test')
     end
 
     it 'allows reading and writing for :year' do
@@ -32,23 +42,23 @@ RSpec.describe 'Car' do
 
   # dot(.) describes class methods
   describe '.colors' do
+    let(:colors) { %w[blue black red green] }
     it 'returns an array of color names' do
-      c = %w[blue black red green]
-      expect(Car.colors).to match_array(c)
+      expect(Car.colors).to match_array(colors)
     end
   end
 
   # Hash(#) describes instance methods
   describe '#full_name' do
+    let(:honda) { Car.new(make: 'Honda', year: 2004, color: 'blue') }
+    let(:new_car) { Car.new }
     it 'returns a string in the expected format' do
-      @honda = Car.new(make: 'Honda', year: 2004, color: 'blue')
-      expect(@honda.full_name).to eq('2004 Honda (blue)')
+      expect(honda.full_name).to eq('2004 Honda (blue)')
     end
 
     context 'when initialized with no arguments' do
       it 'returns a string using default values' do
-        car = Car.new
-        expect(car.full_name).to eq('2007 Volvo (unknown)')
+        expect(new_car.full_name).to eq('2007 Volvo (unknown)')
       end
     end
   end
